286e54e5cbab972a3a5faec096d646ff94998db5
Matthew Irving <matthew.irving@cse-cst.gc.ca>
2022-07-13 19:57:56 -0400

    ♻ Use HTtpClient

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/cli/go-gh"
+	"net/http"
 	"os"
 )
 
@@ -19,8 +21,9 @@ type Repository struct {
 	Owner Owner  `json:"owner"`
 }
 type Subject struct {
-	Url  string `json:"url"`
-	Type string `json:"type"`
+	Url   string `json:"url"`
+	Type  string `json:"type"`
+	Title string `json:"title"`
 }
 type Notification struct {
 	Id         string     `json:"id"`
@@ -39,25 +42,24 @@ func main() {
 
 	httpClient, err := gh.HTTPClient(nil)
 	if err != nil {
-		appError(err, "")
+		appError(err, "unable to get http client")
 	}
 	var firstNotificationPageUrl = fmt.Sprintf("https://api.github.com/notifications?per_page=10?page=1?read=false")
 	notificationResponse, err := httpClient.Get(firstNotificationPageUrl)
-
 	if err != nil {
-		appError(err, "")
+		appError(err, "error getting first page of unread notifications")
 	}
 	var notifications []Notification
 	err = json.NewDecoder(notificationResponse.Body).Decode(&notifications)
 	if err != nil {
-		appError(err, "")
+		appError(err, "unable to decode notification response")
 	}
 	dbg(fmt.Sprintf("Github responded with %d notifications.\n", len(notifications)))
 
 	// Track the unique set of URLs which need to be marked as read.
 	// This will prevent sending multiple requests for the same thread
 	// when the thread has multiple messages
-	var threadUrls = make(map[string]struct{})
+	var uniqueThreads = make(map[string]Notification)
 	for _, notification := range notifications {
 		if notification.Subject.Type == "PullRequest" && notification.Reason == "review_requested" {
 			dbg("Found a notification to mark as read\n")
@@ -68,39 +70,46 @@ func main() {
 			dbg(fmt.Sprintf("Fetching details for pull request %s\n", notification.Subject.Url))
 			pullResponse, err := httpClient.Get(notification.Subject.Url)
 			if err != nil {
-				appError(err, "")
+				appError(err, "unable pull request details")
 			}
 			dbg(fmt.Sprintf("\tResponse status: %s\n", pullResponse.Status))
-			if pullResponse.StatusCode == 200 {
+			if pullResponse.StatusCode == 200 || pullResponse.StatusCode == 404 {
 				var pull Pull
 				err = json.NewDecoder(pullResponse.Body).Decode(&pull)
 				if err != nil {
-					appError(err, "")
+					appError(err, "unable to decode pull request")
 				}
 				dbg(fmt.Sprintf("\tPR state: %s\n", pull.State))
 				if pull.State == "closed" {
-					//
 					dbg(fmt.Sprintf("\tPR url: %s\n", notification.Subject.Url))
-					threadUrls[notification.Url] = struct{}{}
+					uniqueThreads[notification.Url] = notification
 				}
-			} else if pullResponse.StatusCode == 404 {
-				fmt.Printf("Github responded with a 404! Fix this code!\n")
-				os.Exit(1)
-				return
+			} else {
+				appError(errors.New(fmt.Sprintf("unhandled response code %d ", pullResponse.StatusCode)), "unhandled response code for pull request")
 			}
 		}
 	}
 
-	restClient, err := gh.RESTClient(nil)
-	if err != nil {
-		appError(err, "")
-	}
-
-	for url := range threadUrls {
-		err = restClient.Patch(url, nil, nil)
+	for url := range uniqueThreads {
+		var currentThread = uniqueThreads[url]
+		request, err := http.NewRequest("PATCH", url, nil)
 		if err != nil {
-			appError(err, fmt.Sprintf("Unable to mark %s as read", url))
+			appError(err, "Unable to create request object.")
 		}
+		response, err := httpClient.Do(request)
+		if err != nil {
+			appError(err, fmt.Sprintf("Failed to mark notification %s as done.", currentThread.Subject.Title))
+		}
+		if response.StatusCode == 205 {
+			info(fmt.Sprintf("Marked thread [%s] as read.", currentThread.Subject.Title))
+		} else if response.StatusCode == 304 {
+			warn("Thread was not marked as read")
+		} else if response.StatusCode == 403 {
+			appError(errors.New("unauthorized"), "authentication issue, check gh auth")
+		} else {
+			appError(errors.New(fmt.Sprintf("Response was %d", response.StatusCode)), "undocumented status code")
+		}
+		dbg(fmt.Sprintf("The response status was %d\n", response.StatusCode))
 	}
 }
 
@@ -112,6 +121,16 @@ func appError(err error, msg string) {
 	}
 	fmt.Printf("%s\n", err)
 	os.Exit(1)
+}
+
+// info print a message in an info format
+func info(msg string) {
+	fmt.Print(msg)
+}
+
+// info print a message in a warn format
+func warn(msg string) {
+	fmt.Printf(msg)
 }
 
 // if debug is enabled, print a message
